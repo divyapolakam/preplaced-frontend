798db3260ac2dc9b1e55b0fcc3e0ebdf0d70ba3f
unknown <divyapolakam24@gmail.com>
2020-10-30 14:50:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Mainpage from './Components/Mainpage';
+import {BrowserRouter as Router ,Switch , Route} from 'react-router-dom';
+import "./App.css";
+import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<Router>
+<Route path="/" exact component={Mainpage} />
+
+</Router>
+
       </header>
     </div>
-  );
-}
+);
 
+};
 export default App;
+
